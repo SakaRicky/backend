30a94f4ff2452a0ad4a8b20e5bf9ff7706be6a18
Saka Ricky <sakaricky91@gmail.com>
2021-01-30 21:23:39 +0100

    uibuild

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
+require('dotenv').config();
 const express = require('express')
 const app = express();
 const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-const mongodb_password = process.env.MONGO_PASSWORD
-
-console.log("password:", mongodb_password);
-
-const url =
-  `mongodb+srv://rickysaka:${mongodb_password}@cluster0.u3l97.mongodb.net/note-app?retryWrites=true&w=majority`
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean
-})
-
-const Note = mongoose.model('Note', noteSchema);
-
-
+const Note = require('./models/note');
+const morgan = require('morgan');
 
 
 
@@ -47,9 +28,12 @@ let notes = [
     }
   ]
 
+app.use(express.static('build'));
 app.use(express.json()) // for parsing application/json
 app.use(cors());
-app.use(express.static('build'));
+
+morgan.token('body', (request, response) => JSON.stringify(request.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 
 app.get('/', (request, response) => {
@@ -62,47 +46,77 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    note ? response.json(note) : response.status(404).end();
+app.get('/api/notes/:id', (request, response, next) => {
+     Note.findById(request.params.id)
+         .then(requestedNote => {
+          if (requestedNote) {
+            response.json(requestedNote)
+          } else {
+            response.status(404).end()
+          } 
+        })
+        .catch(error => next(err))
   })
 
-const generateID = () => {
-    const maxID = notes.length > 0 ? Math.max(...notes.map(note => note.id)) : 0
-    return maxID + 1;
-}
-
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
 
-    if (!body.content) {
-      return response.status(400).json({
-        error: 'content missing'
-      })
-    }
-
-    const note = {
+    const note = new Note({
       content: body.content,
       important: body.important || false,
-      date: new Date(),
-      id: generateID(),
-    }
+      date: new Date()
+    })
 
-    notes = notes.concat(note);
-    
-    response.json(note);
+    note.save()
+        .then(savedNote => savedNote.toJSON())
+        .then(saveAndFormatedNote => response.json(saveAndFormatedNote))
+        .catch(error => next(error));
+})
+
+
+app.put('/api/notes/:id', (request, response, next) => {
+  console.log('app.put test');
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => response.json(updatedNote))
+      .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter(note => note.id !== id)
 
-    response.status(204).end();
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+          response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: "unknown endpoint"})
+}
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.log('error.message:', error.message);
+
+  if (error.name === 'CastError') {
+    response.status(400).send({error: 'malformated id'})
+  } else if (error.name === "ValidationError") {
+    return response.status(400).send({error: error.message})
+  }
+
+  next(error)
+}
+
 //https://guarded-castle-66521.herokuapp.com/
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
